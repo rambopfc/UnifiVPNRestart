cc3b7aa239ab65d212fca7eea3b4c17f3c500679
Joe <JoeBotta@gmail.com>
2020-08-26 12:00:41 -0400

    provision script v1

--- runonprovision.sh
+++ runonprovision.sh
@@ -1,1 +1,23 @@
-#!/bin/sh
+#!/bin/vbash
+#delete runonprovision task
+
+readonly logFile="/var/log/runonprovision.log"
+
+source /opt/vyatta/etc/functions/script-template
+
+configure > ${logFile}
+delete system task-scheduler task runonprovision  >> ${logFile}
+commit >> ${logFile}
+save >> ${logFile}
+#exit
+
+#download vpn script. cannot create task schedule before file exists
+/usr/bin/curl --silent 'https://raw.githubusercontent.com/rambopfc/UnifiVPNRestart/master/restartvpn.sh' > /config/scripts/restartvpn.sh
+/bin/chmod +x /config/scripts/restartvpn.sh
+
+#create vpn log file
+touch /var/log/vpnlogs.log
+chmod 777 /var/log/vpnlogs.log
+
+#add task to run every day at 3am
+set system task-scheduler task restartvpn crontab-spec "0 3 * * *" executable path /config/scripts/restartvpn.sh
