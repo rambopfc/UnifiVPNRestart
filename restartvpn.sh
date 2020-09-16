ff9a50aa3481dc433c573f787381315d8bf8dbc3
Joe <JoeBotta@gmail.com>
2020-09-16 16:49:17 -0400

    add logging for restart output

--- restartvpn.sh
+++ restartvpn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/opt/vyatta/bin/vyatta-op-cmd-wrapper restart vpn
+/opt/vyatta/bin/vyatta-op-cmd-wrapper restart vpn >> /var/log/vpnlogs.log
 echo $(date -u) "VPN Restarted" >> /var/log/vpnlogs.log
